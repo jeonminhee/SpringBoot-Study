6eec9d0e3c2709683de9e084a7f784c25d25710d
jeonminhee <yycel1357@naver.com>
2021-01-14 15:09:33 +0900

    자바코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,18 +9,16 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
     /* 회원가입 */
-    public Long join(Member member){
+    public Long join(Member member) {
         validateDuplicateMember(member); // 중복회원 검증
         memberRepository.save(member);
         return member.getId();
@@ -34,11 +32,11 @@ public class MemberService {
     }
 
     /* 전체회원 조회 */
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 
